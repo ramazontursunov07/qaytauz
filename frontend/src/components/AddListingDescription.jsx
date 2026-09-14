54408b8b39503c49303e1e466522bca1708ef516
Ramazon Tursunov <288622109+ramazontursunov07@users.noreply.github.com>
2026-09-14 15:05:20 +0500

    Update AddListingDescription.jsx

--- frontend/src/components/AddListingDescription.jsx
+++ frontend/src/components/AddListingDescription.jsx
@@ -8,7 +8,7 @@ const COLORS = {
   chipBg: "#F1F1F1",
 };
 
-cohttp://localhost:8000nst MAX_LENGTH = 1000;
+const MAX_LENGTH = 1000;
 
 export default function AddListingDescription() {
   const navigate = useNavigate();
