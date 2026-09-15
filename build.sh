6749be69fbf85a8e68221a003a66dbe3cc10d1e1
Ramazon Tursunov <288622109+ramazontursunov07@users.noreply.github.com>
2026-09-15 09:38:35 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+user, created = User.objects.get_or_create(username=username, defaults={'email': email})
+user.set_password(password)
+user.is_staff = True
+user.is_superuser = True
+user.save()
+print('Superuser tayyor:', username)
+"
